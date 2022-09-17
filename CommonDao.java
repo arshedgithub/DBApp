a7b63b6b1c06c1b632cfcc68cfa138e6927ed3d7
arshedgithub <arshedahmedmail@gmail.com>
2022-09-17 14:14:35 +0530

    version 5

--- CommonDao.java
+++ CommonDao.java
@@ -2,10 +2,11 @@ import java.sql.*;
 
 public class CommonDao {
     public static ResultSet get(String qry){
+        ResultSet result = null;
         try{
             Connection dbcon = DriverManager.getConnection("jdbc:mysql://localhost:3306/euc1", "root", "1234");
             Statement stm = dbcon.createStatement();
-            ResultSet result = stm.executeQuery(qry);
+            result = stm.executeQuery(qry);
         } catch (SQLException ex){
             System.out.println("Can't get result as " + ex.getMessage());
         }
